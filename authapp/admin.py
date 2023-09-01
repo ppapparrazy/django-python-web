54d37523f41e95dc7e1d3e5a036035d5d5ccf1ee
ppapparrazy <swordfiish2y2@yahoo.com>
2023-09-01 12:32:33 +0300

    File upload

---
+++ authapp/admin.py
@@ -0,0 +1,10 @@
+from authapp.models import Contact, MembershipPlan, Enrollment, Trainer, Gallery, Attendance
+from django.contrib import admin
+
+# Register your models here.
+admin.site.register(Contact)
+admin.site.register(MembershipPlan)
+admin.site.register(Enrollment)
+admin.site.register(Trainer)
+admin.site.register(Gallery)
+admin.site.register(Attendance)
